55294ab6062e68f8ae6570df495913bdd455b22a
Replit user <replituser@example.com>
2021-03-19 19:27:29 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -130,7 +130,8 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
 {
   for(int i = 0; i<2 ; i++)
   {
-    c->datos[i] = (a1 + a2) + (b1 + b2);
+    c->datos[i] = (a1 + a2);
+    
   }
   
 }
