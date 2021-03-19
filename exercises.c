b6301ea2027a2e71dfb424a450bae93b85351768
Replit user <replituser@example.com>
2021-03-19 00:56:14 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -103,7 +103,7 @@ Ejercicio 6.
 Programe la función int obtenerValor(Vector * v, int i), 
 la cual retorna el valor en la posición i del vector v.
 */
-int obtenerValor(Vector * v, int i) 
+int obtenerValor(Vector *v, int i) 
 {
    return v[i].capacidad;
 }
@@ -113,8 +113,8 @@ Ejercicio 7.
 Función que suma los vectores `a` y `b` y 
 actualiza el vector `c` con el resultado de la suma.
 */
-void sumaV(Vector * a, Vector * b, Vector * c) {
-
+void sumaV(Vector *a, Vector *b, Vector *c) 
+{
 }
 
 /*
@@ -122,6 +122,7 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
+{
+  
 }
