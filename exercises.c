667467d1098a9637f2f83ca9e6be60a196c0e8f9
Replit user <replituser@example.com>
2021-03-19 19:28:48 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -129,5 +129,5 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
 {
   c->datos[1] = (a1 + a2);
-
+  c->datos[2] = (b1 + b2);
 }
